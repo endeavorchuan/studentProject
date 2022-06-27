b2ec02a61f14aae87b12cb34cb1c2abdf7116b0d
川 <chc82347794@163.com>
2022-06-27 17:10:06 +0800

    首页 ----- 班级权益页面，布局及样式编写

--- pages/show-contract/show-contract.vue
+++ pages/show-contract/show-contract.vue
@@ -7,8 +7,49 @@
 -->
 
 <template>
-  <view class="contract">
-    班级权益00
+  <view class="show-contract">
+    <view class="contract-header">
+      <image class="image" src="https://fawn.xuexiluxian.cn/api/profile/wechat/bgimg/title.png"></image>
+    </view>
+    <scroll-view class="scroll-view" scroll-y="true">
+      <view class="contract">
+        <view class="show-contract-content">
+          <view class="sign-item">
+            <view class="sign-bg-title">
+              <view class="item-title">实训室</view>
+            </view>
+            <view class="item-content">
+              <view class="item-text">
+                <rich-text>学习周期1个月</rich-text>
+                <rich-text>学习周期1个月</rich-text>
+                <rich-text>学习周期1个月</rich-text>
+              </view>
+            </view>
+            <view class="sign-bg-footer">
+              <rich-text>1234455</rich-text>
+            </view>
+          </view>
+          <view class="sign-item">
+            <view class="sign-bg-title">
+              <view class="item-title">实训室</view>
+            </view>
+            <view class="item-content">
+              <view class="item-text">
+                <rich-text>学习周期1个月</rich-text>
+                <rich-text>学习周期1个月</rich-text>
+                <rich-text>学习周期1个月</rich-text>
+              </view>
+            </view>
+            <view class="sign-bg-footer">
+              <rich-text>1234455</rich-text>
+            </view>
+          </view>
+        </view>
+      </view>
+    </scroll-view>
+    <view class="know">
+      <view class="contract-know">我知道了</view>
+    </view>
   </view>
 </template>
 
@@ -19,5 +60,5 @@ export default {
 </script>
 
 <style scoped>
-
+@import "./css/show-contract.css";
 </style>
