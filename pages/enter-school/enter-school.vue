980dea448318e6bcb8f8f3f96adb1ab54c623251
川 <chc82347794@163.com>
2022-06-29 14:59:20 +0800

    入学信息填写页 ----- 创建页面文件、点击跳转功能实现

---
+++ pages/enter-school/enter-school.vue
@@ -0,0 +1,23 @@
+<!--
+* @name: enter-school
+* @author: chuanchuan
+* @date: 2022-06-29 14:53
+* @description: enter-school
+* @update: 2022-06-29 14:53
+-->
+
+<template>
+  <view class="enter-school">
+    我是快速入学信息填写页面
+  </view>
+</template>
+
+<script>
+export default {
+  name: "enter-school"
+}
+</script>
+
+<style scoped>
+
+</style>
