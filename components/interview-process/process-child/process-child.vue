1bbc0404f66bbad3444cce643718fddb3171e215
川 <chc82347794@163.com>
2022-07-11 11:42:56 +0800

    面试流程页面 ----- 组件内部事件处理(跳转、复制)

--- components/interview-process/process-child/process-child.vue
+++ components/interview-process/process-child/process-child.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: 初浩诚
  * @Date: 2022-07-10 15:22:00
- * @LastEditTime: 2022-07-11 11:20:29
+ * @LastEditTime: 2022-07-11 11:41:09
  * @LastEditors: 初浩诚
  * @Description: 
  * @FilePath: /studentProject/components/interview-process/process-child/process-child.vue
@@ -14,7 +14,7 @@
         <image class="list-item" src="../../../static/imgs/underline.png"></image>
         <view class="list-title">{{ child.title }}</view>
       </view>
-      <view class="list-into">{{ child.reamrk }}</view>
+      <view class="list-into" @tap="goReamrk(child)">{{ child.reamrk }}</view>
     </view>
     <view class="plain-video" v-else>
       <view class="list-plain">
@@ -43,9 +43,47 @@ export default {
       
     }
   },
+  methods: {
+    goReamrk(child) {
+      if (child.objType === 0) {
+        // 面试题
+        uni.switchTab({
+            url: '/pages/interview-question/interview-question'
+        })
+      } else if (child.objType === 1) {
+        // 链接
+        this.copy(child.objPath)
+      }
+    },
+    copy(value) {
+      uni.showModal({
+        content: value,   // 模板中提示的内容
+        confirmText: '复制内容',
+        success: () => {
+          uni.setClipboardData({
+            data: value,    // 被复制的内容
+            success: function () {
+              uni.hideToast({
+                title: '复制成功，请打开百度网盘APP',
+                duration: 2000,
+                icon: 'none'
+              })
+            },
+            fail: function () {
+              uni.hideToast({
+                title: '复制失败',
+                duration: 2000,
+                icon: 'none'
+              })
+            }
+          })
+        }
+      })
+    }
+  },
   components: {
     SyVideo
-  }
+  },
 }
 </script>
 
--- unpackage/dist/dev/mp-weixin/components/interview-process/process-child/process-child.js
+++ unpackage/dist/dev/mp-weixin/components/interview-process/process-child/process-child.js
@@ -65,7 +65,7 @@ __webpack_require__.r(__webpack_exports__);
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0;var SyVideo = function SyVideo() {__webpack_require__.e(/*! require.ensure | components/interview-process/sy-video/sy-video */ "components/interview-process/sy-video/sy-video").then((function () {return resolve(__webpack_require__(/*! @/components/interview-process/sy-video/sy-video.vue */ 239));}).bind(null, __webpack_require__)).catch(__webpack_require__.oe);};var _default =
+/* WEBPACK VAR INJECTION */(function(uni) {Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0;var SyVideo = function SyVideo() {__webpack_require__.e(/*! require.ensure | components/interview-process/sy-video/sy-video */ "components/interview-process/sy-video/sy-video").then((function () {return resolve(__webpack_require__(/*! @/components/interview-process/sy-video/sy-video.vue */ 239));}).bind(null, __webpack_require__)).catch(__webpack_require__.oe);};var _default =
 
 
 
@@ -110,8 +110,47 @@ Object.defineProperty(exports, "__esModule", { value: true });exports.default = 
 
 
   },
+  methods: {
+    goReamrk: function goReamrk(child) {
+      if (child.objType === 0) {
+        // 面试题
+        uni.switchTab({
+          url: '/pages/interview-question/interview-question' });
+
+      } else if (child.objType === 1) {
+        // 链接
+        this.copy(child.objPath);
+      }
+    },
+    copy: function copy(value) {
+      uni.showModal({
+        content: value, // 模板中提示的内容
+        confirmText: '复制内容',
+        success: function success() {
+          uni.setClipboardData({
+            data: value, // 被复制的内容
+            success: function success() {
+              uni.hideToast({
+                title: '复制成功，请打开百度网盘APP',
+                duration: 2000,
+                icon: 'none' });
+
+            },
+            fail: function fail() {
+              uni.hideToast({
+                title: '复制失败',
+                duration: 2000,
+                icon: 'none' });
+
+            } });
+
+        } });
+
+    } },
+
   components: {
     SyVideo: SyVideo } };exports.default = _default;
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["default"]))
 
 /***/ }),
 
